825fac8b0646469a4a0e7f00c996478e3c8cd3fa
Jens Gåsemyr Magnus <jegm@statoil.com>
2018-02-08 13:38:32 +0100

    python 3 ecl_sum

--- python/ecl/test/ecl_mock/ecl_sum_mock.py
+++ python/ecl/test/ecl_mock/ecl_sum_mock.py
@@ -6,14 +6,14 @@ def mock_func(ecl_sum , key , days):
     return days * 10
 
 
-def createEclSum( case , keys , start = datetime.date(2010 , 1, 1) , sim_length_days = 5 * 365 , num_report_step = 5, num_mini_step = 10, dims = (20,10,5) , func_table = {}):
+def createEclSum( case , keys , start = datetime.date(2010 , 1, 1) , sim_length_days = 5 * 365 , num_report_step = 5, num_mini_step = 5, dims = (20,10,5) , func_table = {}):
     ecl_sum = EclSum.writer(case , start , dims[0] , dims[1] , dims[2])
     var_list = []
     for (kw,wgname,num) in keys:
         var_list.append( ecl_sum.addVariable( kw , wgname = wgname , num = num) )
 
-    report_step_length = sim_length_days / num_report_step
-    mini_step_length = report_step_length / num_mini_step
+    report_step_length = sim_length_days / float(num_report_step)
+    mini_step_length = report_step_length / float(num_mini_step)
     for report_step in range(num_report_step):
         for mini_step in range(num_mini_step):
             days = report_step * report_step_length + mini_step * mini_step_length
